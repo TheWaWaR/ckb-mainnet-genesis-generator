c71aa6914dd3e553aff11a98802873c79ab61e10
Qian Linfeng <thewawar@gmail.com>
2019-10-28 02:08:01 +0800

    Cache last round result

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use std::collections::{HashMap, HashSet};
 use std::fmt;
 use std::fs;
 use std::io::{Read, Write};
+use std::path::{Path, PathBuf};
 use std::str::FromStr;
 use std::thread;
 use std::time::Duration;
-use std::path::PathBuf;
 
 use ckb_chain_spec::ChainSpec;
 use ckb_jsonrpc_types::BlockNumber;
@@ -18,10 +18,12 @@ use ckb_types::{
     H160, H256,
 };
 use clap::{App, AppSettings, Arg};
+use serde::{Deserialize, Serialize};
 
 mod config;
 
-const SECP_TYPE_SCRIPT_HASH: H256 = h256!("0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8");
+const SECP_TYPE_SCRIPT_HASH: H256 =
+    h256!("0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8");
 
 fn main() {
     env_logger::init();
@@ -33,7 +35,7 @@ fn main() {
                 .short("R")
                 .takes_value(true)
                 .required(true)
-                .help("Path in config relative path root")
+                .help("Path in config relative path root"),
         )
         .arg(
             Arg::with_name("config")
@@ -73,12 +75,21 @@ fn main() {
 
     let base_spec_path: PathBuf = [root_path, config.base_spec.as_str()].iter().collect();
     let base_spec = read_base_chain_spec(base_spec_path);
+    let testnet_cache_path: Option<PathBuf> = config
+        .testnet_rewards
+        .round5
+        .cache
+        .clone()
+        .map(|filename| [root_path, filename.as_str()].iter().collect());
     let current_testnet_result = read_current_testnet(
         config.testnet_rewards.round5.url.as_str(),
         target_block_number,
+        testnet_cache_path,
     );
     println!("CurrentTestnetResult: {}", current_testnet_result);
-    let round1_path: PathBuf = [root_path, config.testnet_rewards.round1.as_str()].iter().collect();
+    let round1_path: PathBuf = [root_path, config.testnet_rewards.round1.as_str()]
+        .iter()
+        .collect();
     let round1_rewards = read_round1_rewards(round1_path);
     // println!("base_spec: {:?}", base_spec);
     // println!("prev_rewards: {:?}", prev_rewards);
@@ -114,11 +125,33 @@ fn read_round1_rewards(path: PathBuf) -> Vec<(H160, u64)> {
     results
 }
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct CurrentTestnetResult {
-    pub rewards: HashMap<H160, u64>,
+    pub rewards: Vec<(H160, u64)>,
     pub total_base_reward: u64,
     pub last_block_hash: H256,
+    pub last_block_number: u64,
+}
+
+impl CurrentTestnetResult {
+    pub fn new(
+        rewards: HashMap<H160, u64>,
+        total_base_reward: u64,
+        last_block_hash: H256,
+        last_block_number: u64,
+    ) -> Self {
+        let rewards: Vec<(H160, u64)> = rewards.into_iter().collect();
+        CurrentTestnetResult {
+            rewards,
+            total_base_reward,
+            last_block_hash,
+            last_block_number,
+        }
+    }
+
+    pub fn map(&self) -> HashMap<H160, u64> {
+        self.rewards.iter().cloned().collect()
+    }
 }
 
 impl fmt::Display for CurrentTestnetResult {
@@ -133,18 +166,40 @@ impl fmt::Display for CurrentTestnetResult {
     }
 }
 
-fn read_current_testnet(url: &str, block_number: u64) -> CurrentTestnetResult {
+fn read_current_testnet(
+    url: &str,
+    block_number: u64,
+    cache_path: Option<PathBuf>,
+) -> CurrentTestnetResult {
     let mut client = HttpRpcClient::from_uri(url);
-    let mut rewards = HashMap::default();
-    let mut last_block_hash = H256::default();
-    let mut total_base_reward: u64 = 0;
+    let (mut rewards, mut last_block_hash, mut last_block_number, mut total_base_reward) =
+        match cache_path.as_ref() {
+            Some(path) if Path::new(path).exists() => {
+                let file = fs::File::open(path).unwrap();
+                let result: CurrentTestnetResult = serde_json::from_reader(file).unwrap();
+                (
+                    result.map(),
+                    result.last_block_hash.clone(),
+                    result.last_block_number,
+                    result.total_base_reward,
+                )
+            }
+            _ => (HashMap::default(), H256::default(), 0, 0),
+        };
 
     fn get_tip_block_number(client: &mut HttpRpcClient) -> u64 {
         client.get_tip_block_number().call().unwrap().value()
     }
     let mut tip_number = get_tip_block_number(&mut client);
+    log::info!(
+        "tip: {}, last_block_number: {}, last_block_hash: {:#}, total_base_reward: {}",
+        tip_number,
+        last_block_number,
+        last_block_hash,
+        total_base_reward,
+    );
 
-    for number in 1..=block_number {
+    for number in (last_block_number + 1)..=block_number {
         // Wait for next block
         let mut check_round = 0;
         loop {
@@ -174,6 +229,7 @@ fn read_current_testnet(url: &str, block_number: u64) -> CurrentTestnetResult {
         let block_hash = &block.header.hash;
         if number == block_number {
             last_block_hash = block_hash.clone();
+            last_block_number = number;
         }
         let cellbase: packed::Transaction = block.transactions[0].clone().inner.into();
         let lock_script = cellbase
@@ -205,12 +261,31 @@ fn read_current_testnet(url: &str, block_number: u64) -> CurrentTestnetResult {
                 number
             );
         }
+
+        if number % 2000 == 0 && tip_number - number > 300 {
+            if let Some(path) = cache_path.as_ref() {
+                let file = fs::File::create(path).unwrap();
+                let result = CurrentTestnetResult::new(
+                    rewards.clone(),
+                    total_base_reward,
+                    block_hash.clone(),
+                    number,
+                );
+                serde_json::to_writer(file, &result).unwrap();
+                log::info!(
+                    "Current block number: {}, save cache to: {:?}",
+                    number,
+                    path
+                );
+            }
+        }
     }
-    CurrentTestnetResult {
+    CurrentTestnetResult::new(
         rewards,
         total_base_reward,
         last_block_hash,
-    }
+        last_block_number,
+    )
 }
 
 pub struct AddressParser;
